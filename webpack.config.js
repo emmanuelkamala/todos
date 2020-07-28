881f1da8fb62f6092bd5bf995f880815aba788f0
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-07-28 11:33:38 +0300

    Correct Eslint errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,6 @@
 const path = require('path');
 
 module.exports = {
-    parser: "babel-eslint",
-    parserOptions: {
-      sourceType: "module",
-      allowImportExportEverywhere: false,
-      ecmaFeatures: {
-        globalReturn: false,
-      },
-      babelOptions: {
-        configFile: "path/to/config.js",
-      },
-    },
     entry: './src/index.js',
     output: {
         filename: 'main.js',
