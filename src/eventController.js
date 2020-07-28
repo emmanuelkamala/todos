e13c7da683d317d45fb23a547391c3a2505b5a58
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-07-28 22:43:41 +0300

    Edit event controller module

--- src/eventController.js
+++ src/eventController.js
@@ -32,13 +32,13 @@ const controller = (() => {
           renderForm.show('project', selectedId);
           break;
 
-				// Update project
-				case 'button update-project-btn':
-					const project = model.getProject(selectedId);
-					model.updateProject(selectedId);
-					renderProjects.updateProjectBtn(selectedId, project.title);
-					renderForm.remove();
-					break;
+        // Update project
+        case 'button update-project-btn':
+          const project = model.getProject(selectedId);
+          model.updateProject(selectedId);
+          renderProjects.updateProjectBtn(selectedId, project.title);
+          renderForm.remove();
+          break;
 
         // Destroy project
         case 'destroy-project-btn':
@@ -57,76 +57,76 @@ const controller = (() => {
         // Open new item form
         case 'new-item-btn':
           selectedId = document.querySelector('.column-btn-selected').dataset.id;
-					renderForm.remove();
-					renderForm.show('item');
-					break;
+          renderForm.remove();
+          renderForm.show('item');
+          break;
 
-				// Create new item
-				case 'button create-item-btn':
-					if (model.validateItemForm()) {
-						model.createItem(selectedId);
-						renderForm.remove();
-					}
-					renderList.render(selectedId);
-					break;
+        // Create new item
+        case 'button create-item-btn':
+          if (model.validateItemForm()) {
+            model.createItem(selectedId);
+            renderForm.remove();
+          }
+          renderList.render(selectedId);
+          break;
 
-				// Open edit item form
-				case 'item-edit': 
-					itemId = parseInt(e.target.parentNode.attributes[1].value);
-					renderForm.remove();
-					renderForm.show('item', itemId);
-					break;
+        // Open edit item form
+        case 'item-edit': 
+          itemId = parseInt(e.target.parentNode.attributes[1].value);
+          renderForm.remove();
+          renderForm.show('item', itemId);
+          break;
 				
-				// Update item
-				case 'button update-item-btn':
-					model.updateItem(itemId);
-					const data = model.getItemFormData();
-					renderList.updateItemRow(itemId, data);
-					renderForm.remove();
-					break;
+        // Update item
+        case 'button update-item-btn':
+          model.updateItem(itemId);
+          const data = model.getItemFormData();
+          renderList.updateItemRow(itemId, data);
+          renderForm.remove();
+          break;
 
-				// Destroy individual item
-				case 'item-remove':
-					{
-					const itId = parseInt(e.target.parentNode.attributes[1].value);
-					model.destroyItem(itId);
-					renderList.destroyItem(itId);
-					}
-					break;
+        // Destroy individual item
+        case 'item-remove':
+          {
+            const itId = parseInt(e.target.parentNode.attributes[1].value);
+            model.destroyItem(itId);
+            renderList.destroyItem(itId);
+          }
+          break;
 
-				// Cancel and close form
-				case 'button cancel':
-					renderForm.remove();
-					break;
+        // Cancel and close form
+        case 'button cancel':
+          renderForm.remove();
+          break;
 
-				// Select project from sidebar
-				case 'column-btn':
-					renderProjects.toggleProjects(e);
-					selectedId = parseInt(e.target.attributes[1].value);
-					break;
+        // Select project from sidebar
+        case 'column-btn':
+          renderProjects.toggleProjects(e);
+          selectedId = parseInt(e.target.attributes[1].value);
+          break;
 					
-				// Mark item as done
-				case 'item-done': 
-					{
-						const id = parseInt(e.target.attributes[1].value);
-						const item = model.getItem(id);
-						item.done = e.target.checked ? true : false;
-					}
-					break;
-			}
-		});
-	}
+        // Mark item as done
+        case 'item-done': 
+          {
+            const id = parseInt(e.target.attributes[1].value);
+            const item = model.getItem(id);
+            item.done = e.target.checked ? true : false;
+          }
+          break;
+        }
+    });
+  }
 
-	// PRIVATE
-	
-	const _findCurrentId = () => {
-		if (model.projects.length > 0) {
-			const currentProject = document.querySelector('.column-btn-selected');
-			return parseInt(currentProject.dataset.id);
-		}
-	}
+  // PRIVATE
 
-	return { listen };
+  const _findCurrentId = () => {
+    if (model.projects.length > 0) {
+      const currentProject = document.querySelector('.column-btn-selected');
+      return parseInt(currentProject.dataset.id);
+    }
+  }
+
+  return { listen };
 })();
 
 export { controller };
