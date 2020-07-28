19d920eccc6c056770e833168efc9ce86edd344a
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-07-28 11:26:35 +0300

    Corrent linting errors caused by parser

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,17 @@
 const path = require('path');
 
 module.exports = {
-    
+    parser: "babel-eslint",
+    parserOptions: {
+      sourceType: "module",
+      allowImportExportEverywhere: false,
+      ecmaFeatures: {
+        globalReturn: false,
+      },
+      babelOptions: {
+        configFile: "path/to/config.js",
+      },
+    },
     entry: './src/index.js',
     output: {
         filename: 'main.js',
