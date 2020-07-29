448e606a85d486a6eabfe08709727f38ec84221b
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-07-29 19:37:31 +0300

    Change variable naming in event controller module

--- src/eventController.js
+++ src/eventController.js
@@ -9,100 +9,99 @@ const controller = (() => {
   let findCurrentId;
 
   const listen = () => {
-
     window.addEventListener('click', (e) => {
       switch (e.target.className) {
       // Open new project form
-      case 'new-project-btn':
-        renderForm.remove();
-        renderForm.show('project');
-        break;
-      // Create new project
-      case 'button create-project-btn':
-        if (model.validateProjectForm()) {
-          model.createProject();
+        case 'new-project-btn':
           renderForm.remove();
-          renderProjects.renderNew();
+          renderForm.show('project');
+          break;
+      // Create new project
+        case 'button create-project-btn':
+          if (model.validateProjectForm()) {
+            model.createProject();
+            renderForm.remove();
+            renderProjects.renderNew();
+            selectedId = findCurrentId();
+          }
+          break;
+        case 'edit-project-btn':
           selectedId = findCurrentId();
-        }
-        break;
-      case 'edit-project-btn':
-        selectedId = findCurrentId();
-        renderForm.remove();
-        renderForm.show('project', selectedId);
-        break;
+          renderForm.remove();
+          renderForm.show('project', selectedId);
+          break;
       // Update project
-      case 'button update-project-btn':
-        const project = model.getProject(selectedId);
-        model.updateProject(selectedId);
-        renderProjects.updateProjectBtn(selectedId, project.title);
-        renderForm.remove();
-        break;
+        case 'button update-project-btn':
+          const project = model.getProject(selectedId);
+          model.updateProject(selectedId);
+          renderProjects.updateProjectBtn(selectedId, project.title);
+          renderForm.remove();
+          break;
       // Destroy project
-      case 'destroy-project-btn':
-        selectedId = _findCurrentId();
-        if (confirm('Are you sure you want to delete this project?'))
-        {
-          model.destroyProjectItems(selectedId);
-          model.destroyProject(selectedId);
-          renderProjects.destroyProject(selectedId);
-          renderList.clearList();
-          renderProjects.render();
-          selectedId = findCurrentId();
-        }
-        break;
+        case 'destroy-project-btn':
+          selectedId = _findCurrentId();
+          if (confirm('Are you sure you want to delete this project?'))
+          {
+            model.destroyProjectItems(selectedId);
+            model.destroyProject(selectedId);
+            renderProjects.destroyProject(selectedId);
+            renderList.clearList();
+            renderProjects.render();
+            selectedId = findCurrentId();
+          }
+          break;
       // Open new item form
-      case 'new-item-btn':
-        selectedId = document.querySelector('.column-btn-selected').dataset.id;
-        renderForm.remove();
-        renderForm.show('item');
-        break;
+        case 'new-item-btn':
+          selectedId = document.querySelector('.column-btn-selected').dataset.id;
+          renderForm.remove();
+          renderForm.show('item');
+          break;
 
       // Create new item
-      case 'button create-item-btn':
-        if (model.validateItemForm()) {
-          model.createItem(selectedId);
-          renderForm.remove();
-        }
-        renderList.render(selectedId);
-        break;
+        case 'button create-item-btn':
+          if (model.validateItemForm()) {
+            model.createItem(selectedId);
+            renderForm.remove();
+          }
+          renderList.render(selectedId);
+          break;
       // Open edit item form
-      case 'item-edit': 
-        itemId = parseInt(e.target.parentNode.attributes[1].value, 10);
-        renderForm.remove();
-        renderForm.show('item', itemId);
-        break;
+        case 'item-edit': 
+          itemId = parseInt(e.target.parentNode.attributes[1].value, 10);
+          renderForm.remove();
+          renderForm.show('item', itemId);
+          break;
       // Update item
-      case 'button update-item-btn':
-        model.updateItem(itemId);
-        const data = model.getItemFormData();
-        renderList.updateItemRow(itemId, data);
-        renderForm.remove();
-        break;
+        case 'button update-item-btn':
+          model.updateItem(itemId);
+          const data = model.getItemFormData();
+          renderList.updateItemRow(itemId, data);
+          renderForm.remove();
+          break;
       // Destroy individual item
-      case 'item-remove':
-        {
-          const itId = parseInt(e.target.parentNode.attributes[1].value, 10);
-          model.destroyItem(itId);
-          renderList.destroyItem(itId);
-        }
-        break;
+        case 'item-remove':
+          {
+            const itId = parseInt(e.target.parentNode.attributes[1].value, 10);
+            model.destroyItem(itId);
+            renderList.destroyItem(itId);
+          }
+          break;
       // Select project from sidebar
-      case 'column-btn':
-        renderProjects.toggleProjects(e);
-        selectedId = parseInt(e.target.attributes[1].value, 10);
-        break;
+        case 'column-btn':
+          renderProjects.toggleProjects(e);
+          selectedId = parseInt(e.target.attributes[1].value, 10);
+          break;
       // Mark item as done
-      case 'item-done': {
-        const id = parseInt(e.target.attributes[1].value, 10);
-        const item = model.getItem(id);
-        item.done = e.target.checked
-      }
-        break;
+        case 'item-done': {
+          const id = parseInt(e.target.attributes[1].value, 10);
+          const item = model.getItem(id);
+          item.done = e.target.checked
+        }
+          break;
       // Cancel and close form
-      case 'button cancel':
-        renderForm.remove();
-        break;
+        case 'button cancel':
+          renderForm.remove();
+          break;
       }
     });
   };
